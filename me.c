77a3244b765b4fbc49240243daa180dcd674ab2f
Alistair Davies <ali17sam@gmail.com>
2018-04-27 12:45:40 +0100

    some more stuff

---
+++ me.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+    printf("this is even more mediocre than before");
+    printf("\ndont punish me");
+    printf("this is a %c character", 'C');
+    printf("apparntly you cant print a %d", 44);
+    return 0;
+}
