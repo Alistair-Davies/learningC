ab33f011df03a6b63d64edc346d5797bcf00c8d2
Alistair Davies <ali17sam@gmail.com>
2018-04-27 15:52:48 +0100

    lifetime of opportunites

---
+++ KidsandHeros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+main() {
+    char Kid1[12];
+    char Kid3[7]="Andrew";
+    char Kid2[]="Maddie";
+    char Hero1[]="Batman";
+    char Hero2[34]="Spiderman";
+    char Her3[25];
+
+    strcpy(Kid1, "Katie");
+    strcpy(Her3, "the Incredible Hulk");
+    printf("%s\'s favourite hero is %s.\n", Kid1, Hero1);
+    return 0;
+}
